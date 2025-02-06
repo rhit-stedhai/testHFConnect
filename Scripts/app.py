58b703fb489d096d041dc7c3a44ef96b4e7edc46
IanStedham <90013818+rhit-stedhai@users.noreply.github.com>
2025-02-05 19:40:20 -0500

    readding the additional inputs

--- Scripts/app.py
+++ Scripts/app.py
@@ -10,10 +10,10 @@ client = InferenceClient("HuggingFaceH4/zephyr-7b-beta")
 def respond(
     message,
     history: list[tuple[str, str]],
-    system_message="You are a friendly Chatbot.",
-    max_tokens=512,
-    temperature=0.7,
-    top_p=0.95,
+    system_message,
+    max_tokens,
+    temperature,
+    top_p,
 ):
     messages = [{"role": "system", "content": system_message}]
 
@@ -48,18 +48,18 @@ For information on how to customize the ChatInterface, peruse the gradio docs: h
 """
 demo = gr.ChatInterface(
     respond,
-    # additional_inputs=[
-    #     gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
-    #     gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
-    #     gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
-    #     gr.Slider(
-    #         minimum=0.1,
-    #         maximum=1.0,
-    #         value=0.95,
-    #         step=0.05,
-    #         label="Top-p (nucleus sampling)",
-    #     ),
-    # ],
+    additional_inputs=[
+        gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
+        gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
+        gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
+        gr.Slider(
+            minimum=0.1,
+            maximum=1.0,
+            value=0.95,
+            step=0.05,
+            label="Top-p (nucleus sampling)",
+        ),
+    ],
 )
 
 
