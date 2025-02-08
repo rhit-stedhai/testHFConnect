2467a120deb5d7bc82c67579af7d282a146eec7f
IanStedham <90013818+rhit-stedhai@users.noreply.github.com>
2025-02-08 14:04:56 -0500

    adding streaming

--- Scripts/app.py
+++ Scripts/app.py
@@ -60,19 +60,43 @@ import time
 #     # ],
 # )
 
+def print_like_dislike(x: gr.LikeData):
+    print(x.index, x.value, x.liked)
+
+def add_message(history, message):
+    for x in message["files"]:
+        history.append({"role": "user", "content": {"path": x}})
+    if message["text"] is not None:
+        history.append({"role": "user", "content": message["text"]})
+    return history, gr.MultimodalTextbox(value=None, interactive=False)
+
+def bot(history: list):
+    response = "**That's cool!**"
+    history.append({"role": "assistant", "content": ""})
+    for character in response:
+        history[-1]["content"] += character
+        time.sleep(0.05)
+        yield history
+
+
 with gr.Blocks() as demo:
-    chatbot = gr.Chatbot(type="messages")
-    msg = gr.Textbox()
-    clear = gr.ClearButton([msg, chatbot])
+    chatbot = gr.Chatbot(elem_id="chatbot", bubble_full_width=False, type="messages")
 
-    def respond(message, chat_history):
-        bot_message = random.choice(["How are you?", "Today is a great day", "I'm very hungry"])
-        chat_history.append({"role": "user", "content": message})
-        chat_history.append({"role": "assistant", "content": bot_message})
-        time.sleep(2)
-        return "", chat_history
+    chat_input = gr.MultimodalTextbox(
+        interactive=True,
+        file_count="multiple",
+        placeholder="Enter message or upload file...",
+        show_label=False,
+        sources=["microphone", "upload"],
+    )
 
-    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+    chat_msg = chat_input.submit(
+        add_message, [chatbot, chat_input], [chatbot, chat_input]
+    )
+    bot_msg = chat_msg.then(bot, chatbot, chatbot, api_name="bot_response")
+    bot_msg.then(lambda: gr.MultimodalTextbox(interactive=True), None, [chat_input])
+
+    chatbot.like(print_like_dislike, None, None, like_user_message=True)
 
 if __name__ == "__main__":
     demo.launch()
